fb9acf9d11de05b3b2998660ebbed077451740a0
Acapellia <longchim@naver.com>
2020-11-27 14:50:43 +0900

    AlgorithmStudy_201127_Python

--- Python/Code2011/201118_Curriculum.py
+++ Python/Code2011/201118_Curriculum.py
@@ -9,6 +9,35 @@ for i in range(1, n+1):
     data = list(map(int, input().split()))
     time[i] = data[0]
     for d in data[1:-1]:
+        graph[d].append(i)
+        indegree[i] += 1
+
+q = deque()
+result = copy.deepcopy(time)
+for i in range(1,n+1):
+    if indegree[i] == 0:
+        q.append(i)
+
+print("graph")
+print(graph)
+print("--------")
+print("indegree")
+for i in range(1, n+1):
+    print(indegree[i])
+print("--------")
+while q:
+    temp = q.popleft()
+    print(f"{temp}'s connected")
+    for x in graph[temp]:
+        print(x)
+        result[x] = max(result[temp]+time[x], result[x])
+        indegree[x] -= 1
+        if indegree[x] == 0:
+            q.append(x)
+    print("----------")
+
+for i in range(1, n+1):
+    print(result[i])
 
 '''
 5
